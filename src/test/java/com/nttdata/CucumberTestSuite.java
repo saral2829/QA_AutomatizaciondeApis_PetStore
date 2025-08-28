f6a7f5ccaa0adbe2d16f75a49025e1f0f545061e
lgomez <sarybell28@gmail.com>
2025-08-27 20:28:28 -0500

    Modificacion en el runner

--- src/test/java/com/nttdata/CucumberTestSuite.java
+++ src/test/java/com/nttdata/CucumberTestSuite.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {"pretty"},
         features = "classpath:features",
-        tags = "@Test"   //  ==> Definir el @tag  a ejecutar
+        tags = "@TestCrearOrder"   //  @TestConsultarOrder ==> Definir el @tag  a ejecutar
 )
 public class CucumberTestSuite {
 }
